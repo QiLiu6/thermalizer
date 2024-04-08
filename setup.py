3a495012df37327bbf176d4a7d2b67b075833533
Chris-Pedersen <c.pedersen@nyu.edu>
2024-04-08 17:35:32 -0400

    Basic outline

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+description = "Thermalizer"
+version="0.0.1"
+
+setup(name="thermalizer",
+    version=version,
+    description=description,
+    url="https://github.com/Chris-Pedersen/thermalizer",
+    author="Chris Pedersen",
+    author_email="c.pedersen@nyu.edu",
+    packages=find_packages(),
+    )
