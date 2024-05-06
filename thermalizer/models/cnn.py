3c332f1949a294db1a645cf81e5d3dfcbb36c2c0
Chris-Pedersen <c.pedersen@nyu.edu>
2024-05-06 17:49:08 -0400

    Add vector-input CNN

--- thermalizer/models/cnn.py
+++ thermalizer/models/cnn.py
@@ -103,3 +103,66 @@ class FCNN(nn.Module):
             pickle.dump(save_dict, handle, protocol=pickle.HIGHEST_PROTOCOL)
         print("Model saved as %s" % save_string)
         return
+
+
+class VectorCNN(nn.Module):
+    def __init__(self,config):
+        '''
+        Packs sequence of n_conv=config["conv_layers"] convolutional layers in a list.
+        First layer has config["input_channels"] input channels, and last layer has
+        config["output_channels"] output channels
+        '''
+        super().__init__()
+        self.config=config
+        self.image_res=config["image_size"]
+        self.config["arch"]="VectorCNN"
+
+        blocks = []
+        ## If the conv_layers key is missing, we are running
+        ## with an 8 layer CNN
+        if ("conv_layer" in self.config) == False:
+            self.config["conv_layers"]=8
+        ## Back batch_norm toggle backwards compatible - models trained pre-3rd April 2024
+        ## won't have a batch_norm entry - they will all just have batch_norm though
+        if ("batch_norm" in self.config) == False:
+            self.config["batch_norm"]=True
+        blocks.extend(make_block(self.config["input_channels"],128,5,self.config["activation"],batch_norm=self.config["batch_norm"])) #1
+        blocks.extend(make_block(128,64,5,self.config["activation"],batch_norm=self.config["batch_norm"]))                            #2
+        if self.config["conv_layers"]==3:
+            blocks.extend(make_block(64,self.config["output_channels"],3,'False',False))
+        elif self.config["conv_layers"]==4:
+            blocks.extend(make_block(64,32,3,self.config["activation"],batch_norm=self.config["batch_norm"]))                            
+            blocks.extend(make_block(32,self.config["output_channels"],3,'False',False))
+        else: ## 5 layers or more
+            blocks.extend(make_block(64,32,3,self.config["activation"],batch_norm=self.config["batch_norm"])) ## 3rd layer
+            for aa in range(4,config["conv_layers"]):
+                ## 4th and above layer
+                blocks.extend(make_block(32,32,3,self.config["activation"],batch_norm=self.config["batch_norm"]))
+            ## Output layer
+            blocks.extend(make_block(32,self.config["output_channels"],3,'False',False))
+        self.conv = nn.Sequential(*blocks)
+
+    def forward(self, x):
+        batchsize=x.shape[0]
+        #if len(x.shape)==3:
+        #    x=x.unsqueeze(0)
+        x=x.view(batchsize,1,self.image_res,self.image_res)
+        
+        x = self.conv(x)
+        x=x.view(batchsize,self.image_res*self.image_res)
+        return x
+
+    def save_model(self):
+        """ Save the model config, and optimised weights and biases. We create a dictionary
+        to hold these two sub-dictionaries, and save it as a pickle file """
+        if self.config["save_path"] is None:
+            print("No save path provided, not saving")
+            return
+        save_dict={}
+        save_dict["state_dict"]=self.state_dict() ## Dict containing optimised weights and biases
+        save_dict["config"]=self.config           ## Dict containing config for the dataset and model
+        save_string=os.path.join(self.config["save_path"],self.config["save_name"])
+        with open(save_string, 'wb') as handle:
+            pickle.dump(save_dict, handle, protocol=pickle.HIGHEST_PROTOCOL)
+        print("Model saved as %s" % save_string)
+        return
