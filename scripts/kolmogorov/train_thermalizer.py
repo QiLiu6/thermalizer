4cf29bd7279cedff01d559f941b230bdfb1f5c2c
Chris-Pedersen <c.pedersen@nyu.edu>
2024-07-25 10:12:39 -0400

    Add thermalizer training script

--- scripts/kolmogorov/train_thermalizer.py
+++ scripts/kolmogorov/train_thermalizer.py
@@ -11,51 +11,85 @@ import torch.nn as nn
 from torch.optim import AdamW
 from torch.optim.lr_scheduler import OneCycleLR
 from torch.utils.data import DataLoader
-from torch.utils.data.sampler import SubsetRandomSampler
+from torch.utils.data.sampler import RandomSampler
 
 import thermalizer.models.diffusion as diffusion
 import thermalizer.models.unet as unet
 import thermalizer.dataset.datasets as datasets
 import thermalizer.kolmogorov.util as util
 
-import argparse
+config={}
+config["input_channels"]=1
+config["output_channels"]=1
+config["num_blocks"]=[2,2,2]
+config["hidden_channels"]=32
+config["time_embedding_dim"]=128
+config["image_size"]=64
+config["noise_sampling_coeff"]=0.35
+config["denoise_time"]=400
+config["activation"]="gelu"
+config["norm"]=False
+config["file_path"]="/scratch/cp3759/thermalizer_data/kolmogorov/reynolds10k/diff.p"
+config["whitening"]="/scratch/cp3759/thermalizer_data/kolmogorov/reynolds10k/white_10k.pt"
+config["input_channels"]=1
+config["output_channels"]=1
+config["subsample"]=50000
+config["save_name"]="model_weights.pt"
+config["dim_mults"]=[2,4]
+config["base_dim"]=32
+config["timesteps"]=1000
+#config["add_noise"]=1e-4
+config["optimization"]={}
+config["optimization"]["epochs"]=30
+config["optimization"]["lr"]=0.001
+config["optimization"]["wd"]=0.05
+config["optimization"]["batch_size"]=64
+config["optimization"]["scheduler"]=True
 
-
-parser = argparse.ArgumentParser()
-parser.add_argument(
-    "--config", required=True, type=str, help="name of config file"
-)
-args = parser.parse_args()
-
-config = yaml.safe_load(Path(args.config).read_text())
 print(config)
 
-ds=datasets.KolmogorovDataset(config["data_path"],subsample=config["subsample"])
+train_data,valid_data,config=datasets.parse_data_file(config)
+
+ds_train=datasets.FluidDataset(train_data)
+ds_valid=datasets.FluidDataset(valid_data)
 
 train_loader = DataLoader(
-    ds,
+    ds_train,
     num_workers=10,
     batch_size=config["optimization"]["batch_size"],
-    sampler=SubsetRandomSampler(ds.train_idx),
+    sampler=RandomSampler(ds_train),
 )
-
 valid_loader = DataLoader(
-    ds,
+    ds_valid,
     num_workers=10,
     batch_size=config["optimization"]["batch_size"],
-    sampler=SubsetRandomSampler(ds.valid_idx),
+    sampler=RandomSampler(ds_valid),
 )
-
-config["train_fields"]=len(ds.train_idx)
-config["valid_fields"]=len(ds.valid_idx)
-config["field_std"]=ds.x_std
 
 device="cuda"
 
 ## Set up fourier grid for calculating KE of validation imgs
 grid=util.fourierGrid(64)
 
-wandb.init(project="kol_diffusion",entity="m2lines",config=config,dir="/scratch/cp3759/pyqg_data/wandb_runs")
+train_loader = DataLoader(
+    ds_train,
+    num_workers=10,
+    batch_size=config["optimization"]["batch_size"],
+    sampler=RandomSampler(ds_train),
+)
+valid_loader = DataLoader(
+    ds_valid,
+    num_workers=10,
+    batch_size=config["optimization"]["batch_size"],
+    sampler=RandomSampler(ds_valid),
+)
+
+device="cuda"
+
+## Set up fourier grid for calculating KE of validation imgs
+grid=util.fourierGrid(64)
+
+wandb.init(project="kol_diffusion",entity="chris-pedersen",config=config,dir="/scratch/cp3759/pyqg_data/wandb_runs")
 
 ## Make sure save path, train set size, wandb url are passed to config before model is initialised!
 ## otherwise these important things aren't part of the model config property
@@ -74,87 +108,64 @@ scheduler=OneCycleLR(optimizer,config["optimization"]["lr"],total_steps=config["
 loss_fn=nn.MSELoss(reduction='mean')
 
 global_steps=0
-for i in range(1,config["optimization"]["epochs"]+1):
-    train_running_loss = 0.0
+for i in range(1,config["optimization"]["epochs"]):
     model.train()
     ## Loop over batches
     for j,image in enumerate(train_loader):
-        image=image[:,0,:,:].unsqueeze(1).to(device)
+        image=image.to(device)
         noise=torch.randn_like(image).to(device)
-        pred=model(image,noise)
+        pred,_,_=model(image,noise)
         loss=loss_fn(pred,noise)
         loss.backward()
         optimizer.step()
         optimizer.zero_grad()
         scheduler.step()
         global_steps+=1
-    
+
     ## Push loss values for each epoch to wandb
     log_dic={}
     log_dic["epoch"]=i
     log_dic["training_loss"]=(loss.detach().cpu().item()/(len(image)))
 
-    ## Validation stuff
-    model.eval()
     valid_imgs=next(iter(valid_loader))
-    valid_imgs=valid_imgs[:,0,:,:].unsqueeze(1).to(device)
-    noise=torch.randn_like(valid_imgs).to(device)
-    t=(torch.ones(len(valid_imgs),dtype=torch.int64)*config["denoise_time"]).to(device)
-    
-    noised=model._forward_diffusion(valid_imgs,t,noise)
-    denoised=model.denoising(noised,config["denoise_time"])
-    
+    valid_imgs=valid_imgs.to(device)
+    denoised,noised=model.denoising(valid_imgs,config["denoise_time"])
+
     ## Get losses of noise level and denoised images
     noise_loss=loss_fn(valid_imgs,noised)
     valid_denoise_loss=loss_fn(valid_imgs,denoised)
-    
-    valid_imgs=valid_imgs.to("cpu")
-    noised=noised.to("cpu")
-    denoised=denoised.to("cpu")
-    
-    index=np.random.randint(config["optimization"]["batch_size"])
-    fig_plot=plt.figure(figsize=(14,3))
-    plt.suptitle("Denoising held-outs at epoch %d" % i)
-    plt.subplot(1,4,1)
-    plt.title("True")
-    plt.imshow(valid_imgs[index].squeeze().cpu(),cmap=sns.cm.icefire)
-    plt.colorbar()
-    
-    plt.subplot(1,4,2)
-    plt.title("Noised")
-    plt.imshow(noised[index].squeeze().cpu(),cmap=sns.cm.icefire)
-    plt.colorbar()
-    
-    plt.subplot(1,4,3)
-    plt.title("Denoised")
-    plt.imshow(denoised[index].squeeze().cpu(),cmap=sns.cm.icefire)
-    plt.colorbar()
-    
-    plt.subplot(1,4,4)
-    plt.title("Residual")
-    plt.imshow(valid_imgs[index].squeeze().cpu()-denoised[index].squeeze().cpu(),cmap=sns.cm.icefire)
-    plt.colorbar()
 
-    denoise_fig=wandb.Image(fig_plot)
-    wandb.log({"Denoise":denoise_fig})
-    
+    fig_fields=plt.figure(figsize=(18,9))
+    plt.suptitle("Original (top), Noised (middle), Denoised (lower)")
+    for aa in range(5):
+        plt.subplot(3,5,aa+1)
+        plt.imshow(valid_imgs[aa].squeeze().cpu(),cmap=sns.cm.icefire)
+        plt.colorbar()
+        plt.subplot(3,5,aa+6)
+        plt.imshow(noised[aa].squeeze().cpu(),cmap=sns.cm.icefire)
+        plt.colorbar()
+        plt.subplot(3,5,aa+11)
+        plt.imshow(denoised[aa].squeeze().cpu(),cmap=sns.cm.icefire)
+        plt.colorbar()
+    plt.tight_layout()
+    wandb.log({"Fields":wandb.Image(fig_fields)})
+    plt.close()
+
     fig_ke=plt.figure()
     plt.title("KE spectra: black=true, red=noised, blue=denoised")
     for aa in range(len(valid_imgs)):
-        k1d_plot,ke=util.get_ke(valid_imgs[aa].squeeze(),grid)
+        k1d_plot,ke=util.get_ke(valid_imgs[aa].squeeze().cpu(),grid)
         plt.loglog(k1d_plot,ke,color="black",alpha=0.1)
-        k1d_plot,ke=util.get_ke(noised[aa].squeeze(),grid)
+        k1d_plot,ke=util.get_ke(noised[aa].squeeze().cpu(),grid)
         plt.loglog(k1d_plot,ke,color="red",alpha=0.1)
-        k1d_plot,ke=util.get_ke(denoised[aa].squeeze(),grid)
+        k1d_plot,ke=util.get_ke(denoised[aa].squeeze().cpu(),grid)
         plt.loglog(k1d_plot,ke,color="blue",alpha=0.1)
-
-    spectra_fig=wandb.Image(fig_ke)
-    wandb.log({"Spectra":spectra_fig})
-
+    wandb.log({"Spectra":wandb.Image(fig_ke)})
+    plt.close()
+    
     log_dic["denoise_loss"]=(valid_denoise_loss.detach().cpu().item()/(len(valid_imgs)))
     log_dic["noise_loss"]=(noise_loss.detach().cpu().item()/(len(valid_imgs)))
     wandb.log(log_dic)
-    plt.close()
 
 model.model.save_model()
 wandb.finish()
